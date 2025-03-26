380ea0d705601fa3fc568c2a953d638f92be544f
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2025-03-26 15:04:57 +0900

    unittests: queue: Add test for underflow queue
    Verifies dequeue from an empty queue (underflow).
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- unittests/queue.c
+++ unittests/queue.c
@@ -74,6 +74,32 @@ START_TEST(test_queue_empty)
 }
 END_TEST
 
+START_TEST(test_queue_dequeue_underflow)
+{
+	char item1[] = "abc";
+	char item2[sizeof(item1)];
+
+	int qlength = 10;
+	int buf_size = qlength * sizeof(item1);
+	char buf[buf_size];
+
+	csp_queue_handle_t qh;
+	csp_static_queue_t q;
+
+	/* zero clear */
+	memset(buf, 0, buf_size);
+	memset(item2, 0, sizeof(item2));
+
+	csp_init();
+
+	/* create */
+	qh = csp_queue_create_static(qlength, sizeof(item1), buf, &q);
+
+	/* The queue is empty, so dequeue operation throws an error */
+	ck_assert_int_eq(csp_queue_dequeue(qh, item2, 1000), CSP_QUEUE_ERROR);
+}
+END_TEST
+
 Suite * queue_suite(void)
 {
 	Suite *s;
@@ -84,6 +110,7 @@ Suite * queue_suite(void)
 	tc_free = tcase_create("free");
 	tcase_add_test(tc_free, test_queue_free_707);
 	tcase_add_test(tc_free, test_queue_empty);
+	tcase_add_test(tc_free, test_queue_dequeue_underflow);
 	suite_add_tcase(s, tc_free);
 
 	return s;
