278d43ee46a0d2c41deaa6939ee274ed0c38d535
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2025-04-02 23:22:39 +0900

    csp_port: Fix function argument name mismatch
    Fix function argument name mismatch in csp_port_get_socket.
    
    Updated the argument name from 'dport' to 'port' in the function declaration to
    match its definition.
    
    This fixes a style issue detected by static analysis.
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- src/csp_port.h
+++ src/csp_port.h
@@ -4,5 +4,6 @@
 
 #include <csp/csp_types.h>
 
-csp_socket_t * csp_port_get_socket(unsigned int dport);
+csp_socket_t * csp_port_get_socket(unsigned int port);
+
 csp_callback_t csp_port_get_callback(unsigned int port);
